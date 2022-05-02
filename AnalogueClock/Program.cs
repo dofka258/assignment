37104d273e85145229cd98edc9951bb50b4b2a36
dovyd <dovyd@LAPTOPDFK>
2022-05-02 23:00:47 +0300

    Add project files.

---
+++ AnalogueClock/Program.cs
@@ -0,0 +1,63 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int hours = -1;
+        int minutes = -1;
+        Console.WriteLine("Hi ReizTech!");
+        while (hours == -1)
+        {
+            Console.WriteLine("Input hours:");
+            var input = Convert.ToInt32(Console.ReadLine());
+            if (input >= 0 && input <= 23)
+                hours = input;
+            else Console.WriteLine("Try again...");
+        }
+        while (minutes == -1)
+        {
+            Console.WriteLine("Input minutes:");
+            var input = Convert.ToInt32(Console.ReadLine());
+            if (input >= 0 && input <= 59)
+                minutes = input;
+            else Console.WriteLine("Try again...");
+        }
+        Console.WriteLine($"The lesser degree between the arrows at {String.Format("{0:00}", hours)}:{String.Format("{0:00}", minutes)} " +
+                            $"is {GetLesserAngle(GetDegreesOfMinutes(minutes), GetDegreesOfHours(hours, minutes))}°");
+    }
+
+    private static double GetDegreesOfMinutes(int minutes) => 360/60*minutes;
+    private static double GetDegreesOfHours(int hours, int minutes) => hours<12 ? 360/12*hours+(double)(360/12)/60*minutes : 360/12*(hours-12)+(double)(360/12)/60*minutes;
+    private static double GetLesserAngle(double minuteDegree, double hourDegree)
+    {
+        double result = 0;
+        if (minuteDegree > hourDegree)
+        {
+            result = minuteDegree - hourDegree;
+            if (result <= 360 / 2)
+                return result;
+            return 360 - minuteDegree + hourDegree;
+        }
+        else if (minuteDegree < hourDegree)
+        {
+            result = hourDegree - minuteDegree;
+            if (result <= 360 / 2)
+                return result;
+            return 360 - hourDegree + minuteDegree;
+        }
+        else if (minuteDegree == hourDegree)
+            return 0;
+
+        return result;
+    }
+
+    //Method I used to test application
+    private static void Test(int testHour)
+    {
+        string h = String.Format("{0:00}", testHour);
+        for (int i = 0; i <= 59; i++)
+        {
+            string min = String.Format("{0:00}", i);
+            Console.WriteLine($"The lesser degree between the arrows at {h}:{min} is {GetLesserAngle(GetDegreesOfMinutes(i), GetDegreesOfHours(testHour, i))}°");
+        }
+    }
+}
---
+++ HierarchicalStructure/Program.cs
@@ -0,0 +1,69 @@
+﻿using HierarchicalStructure;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hi ReizTech!");
+        Branch tree = PopulatedTreeByExample(new Branch());
+        var depth = DepthOfTree(tree);
+        Console.WriteLine($"The depth of example structure is {depth}");
+        Console.WriteLine("How many randomly generated structures would you like to see?");
+        int generateStructures = Convert.ToInt32(Console.ReadLine());
+        if(generateStructures > 0)
+        for(int i = 0; i < generateStructures; i++)
+        {
+            tree = PopulatedRandomTree(new Branch(), 0, 12);
+            depth = DepthOfTree(tree);
+            Console.WriteLine($"The depth of a randomly generated structure ({i+1}/{generateStructures}) is {depth}");
+            Task.Delay(1000).Wait();
+        }
+    }
+
+    private static int DepthOfTree(Branch tree)
+    {
+        if (tree.branches.Count == 0)
+            return 1;
+
+        var maxDepth = 0;
+        foreach (var branch in tree.branches)
+        {
+            var tempDepth = DepthOfTree(branch);
+            if (tempDepth > maxDepth)
+                maxDepth = tempDepth;
+        }
+        return maxDepth + 1;
+    }
+
+    private static Branch PopulatedRandomTree(Branch tree, int iteration, int maxIteration)
+    {
+        if(iteration >= maxIteration - 1)
+            return tree;
+
+        Random rnd = new Random();
+        int random = rnd.Next(0, 3);
+        for (int i = 0; i < random; i++)
+            tree.branches.Add(new Branch());
+
+        int branchAmount = tree.branches.Count;
+        for (int i = 0; i < branchAmount; i++)
+            tree.branches[i] = PopulatedRandomTree(new Branch(), iteration+1, maxIteration);
+        return tree;
+    }
+
+    private static Branch PopulatedTreeByExample(Branch tree)
+    {
+        tree.branches.Add(new Branch());
+        tree.branches.Add(new Branch());
+        tree.branches[0].branches.Add(new Branch());
+        tree.branches[1].branches.Add(new Branch());
+        tree.branches[1].branches.Add(new Branch());
+        tree.branches[1].branches.Add(new Branch());
+        tree.branches[1].branches[0].branches.Add(new Branch());
+        tree.branches[1].branches[1].branches.Add(new Branch());
+        tree.branches[1].branches[1].branches.Add(new Branch());
+        tree.branches[1].branches[1].branches[0].branches.Add(new Branch());
+        return tree;
+    }
+}
+
